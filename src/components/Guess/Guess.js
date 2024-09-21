193a5df6b3bcee8a89c5d9dbd0d1c83d24f126ce
Tomi Duyile <thecodingpoet@gmail.com>
2024-09-21 14:35:41 +0100

    Refactor code

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { range } from '../../utils'
-import { NUM_OF_GUESSES_ALLOWED, EMPTY_GUEST_CELLS } from '../../constants'
+import { NUM_OF_GUESSES_ALLOWED, LENGTH_OF_GUESS } from '../../constants'
 import { checkGuess } from '../../game-helpers'
 
 function Guess({ guessHistory, answer }) {
@@ -8,16 +8,17 @@ function Guess({ guessHistory, answer }) {
     <div className='guess-results'>
       {range(0, NUM_OF_GUESSES_ALLOWED).map((index) => {
         const guess = guessHistory[index]
-        const guessCells = guess ? guess.split('') : EMPTY_GUEST_CELLS
-
         const checkedGuess = checkGuess(guess, answer)
 
         return (
           <p key={index} className='guess'>
-            {guessCells.map((cell, index) => {
-              const { status } = checkedGuess?.[index] || {}
-              return <span className={`cell ${status}`}>{cell}</span>
-            })}
+            {range(0, LENGTH_OF_GUESS).map((index) =>
+              checkedGuess ? (
+                <span className={`cell ${checkedGuess[index].status}`}>{checkedGuess[index].letter}</span>
+              ) : (
+                <span className='cell'></span>
+              )
+            )}
           </p>
         )
       })}
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const NUM_OF_GUESSES_ALLOWED = 6
-export const EMPTY_GUEST_CELLS = new Array(5).fill('')
+export const LENGTH_OF_GUESS = 5
